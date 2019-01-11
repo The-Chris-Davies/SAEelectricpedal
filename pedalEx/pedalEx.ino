fcc3f3b51819e1e36778e53ba6fe0dcc06e2bffa
Chris Davies <chriskd662@gmail.com>
2019-01-11 13:24:21 -0500

    implemented watchdog timer

--- pedalEx/pedalEx.ino
+++ pedalEx/pedalEx.ino
@@ -3,6 +3,7 @@ map linear pot to rotary pot during calibration, then run checks to see if map f
 
 */
 #include "pedal.h"
+#include <libmaple/iwdg.h>
 
 #define ROTARYPIN PA2
 #define LINEARPIN PA1
@@ -15,31 +16,34 @@ void setup() {
 	Serial.begin(9600);
 	delay(2000);
 }
+
 short val;
 
 void loop() {
 	Pedal ped(ROTARYPIN, LINEARPIN);
-	bool calibFlag = true;
-	while(1){
-		//begin and initialize pedal controller
-		if(digitalRead(PB13) && calibFlag){
-			Serial.println("calibrating");
-			ped.calibrate(PB13);
-			calibFlag = false;
-			Serial.println("done calib");
-		}
-		
+
+	//begin and initialize pedal controller
+	if(digitalRead(PB13) && calibFlag){
+		Serial.println("calibrating");
+		ped.calibrate(PB13);
+		Serial.println("done calib");
+	}
+
+	iwdg_init(IWDG_PRE_32, 10);	//init watchdog with value of ~8ms
+
+	while(1):
+		//reset watchdog
+		iwdg_feed();
+
 		//continuously read and print pedal value
-		if(digitalRead(PB14)){
-			val = ped.read();
-			if(val == -1){
-				Serial.print ("Pedal Read Error!");
-				analogWrite(APPSOUT, 0)
-			}
-			else {
-				Serial.println(val);
-				analogWrite(APPSOUT, val);
-			}
+		val = ped.read();
+		if(val == -1){
+			Serial.print ("Pedal Read Error!");
+			analogWrite(APPSOUT, 0)
+		}
+		else {
+			Serial.println(val);
+			analogWrite(APPSOUT, val);
 		}
 	}
 }
